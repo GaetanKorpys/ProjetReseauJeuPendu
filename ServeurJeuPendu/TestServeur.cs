97b42e611d2323c7a7764d2615a1dd69c6f8dabf
Gaetan <gaetan.korpys@hotmail.fr>
2022-11-01 13:39:12 +0100

    Add 2 projects

---
+++ ServeurJeuPendu/TestServeur.cs
@@ -0,0 +1,18 @@
+﻿using ServerJeuPendu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServeurJeuPendu
+{
+    internal class TestServeur
+    {
+        static void Main(string[] args)
+        {
+            Serveur serveur = new Serveur();
+            serveur.ExecuteServeur();
+        }
+    }
+}
